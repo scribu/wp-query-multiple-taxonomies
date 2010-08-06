c2a99bf76766c132a34ca2c2cd518b5ccf1d1267
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-06 23:01:18 +0000

    bump to 1.3-alpha5
    git-svn-id: http://plugins.svn.wordpress.org/query-multiple-taxonomies/trunk@272662 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- query-multiple-taxonomies.php
+++ query-multiple-taxonomies.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Query Multiple Taxonomies
-Version: 1.3-alpha4
+Version: 1.3-alpha5
 Description: Filter posts through multiple custom taxonomies
 Author: scribu
 Author URI: http://scribu.net
